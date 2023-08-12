aee8b322af5af021820e34a145101fd11d011761
Shane Martin <shane.martin@plooto.com>
2023-08-12 11:41:43 -0400

    chore: convert ESLint config to CJS, add custom rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint'],
+
+  parser: '@typescript-eslint/parser',
+  root: true,
+
+  rules: {
+    'import/extensions': 'warn',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-unresolved': 'off',
+
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prefer-stateless-function': 'warn',
+
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
