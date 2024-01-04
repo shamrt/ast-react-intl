8d455d12c2156b24025ac0032c6ce57502db0a39
Shane Martin <shane.martin@plooto.com>
2024-01-04 13:24:30 -0500

    refactor: simplify `intlTransformerCodemod` module

--- src/__test__/i18nTransfomerCodemod.test.ts
+++ src/__test__/i18nTransfomerCodemod.test.ts
@@ -2,19 +2,25 @@
 import { defineTest } from 'jscodeshift/dist/testUtils';
 
 describe('intlTransformerCodemod', () => {
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Diacritics');
-  // defineTest(__dirname, 'intlTransformerCodemod', null, 'ExpressionContainer');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Functional');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Props');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Tsx');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Yup');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'CallExpression');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Imported');
-  // defineTest(__dirname, 'intlTransformerCodemod', null, 'WithHoc');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'NoChange');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Hooks');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'HooksInlineExport');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Classnames');
-  defineTest(__dirname, 'intlTransformerCodemod', null, 'Svg');
-  // defineTest(__dirname, 'intlTransformerCodemod', null, 'Parameters');
+  const FIXTURE_FILENAMES = [
+    'Diacritics',
+    // 'ExpressionContainer',
+    'Functional',
+    'Props',
+    'Tsx',
+    'Yup',
+    'CallExpression',
+    'Imported',
+    // 'WithHoc',
+    'NoChange',
+    'Hooks',
+    'HooksInlineExport',
+    'Classnames',
+    'Svg',
+    // 'Parameters',
+  ];
+
+  FIXTURE_FILENAMES.forEach((filename) => {
+    defineTest(__dirname, 'intlTransformerCodemod', null, filename);
+  });
 });
