9af56332a90aea9f481eb15a0aada95c3ca64c96
Shane Martin <shane.martin@plooto.com>
2024-01-04 09:07:45 -0500

    test: update Diacritics test

--- src/__test__/i18nTransfomerCodemod.test.ts
+++ src/__test__/i18nTransfomerCodemod.test.ts
@@ -2,7 +2,7 @@
 import { defineTest } from 'jscodeshift/dist/testUtils';
 
 describe('intlTransformerCodemod', () => {
-  // defineTest(__dirname, 'intlTransformerCodemod', null, 'Diacritics');
+  defineTest(__dirname, 'intlTransformerCodemod', null, 'Diacritics');
   // defineTest(__dirname, 'intlTransformerCodemod', null, 'ExpressionContainer');
   defineTest(__dirname, 'intlTransformerCodemod', null, 'Functional');
   // defineTest(__dirname, 'intlTransformerCodemod', null, 'Props');
--- src/__testfixtures__/Diacritics.output.tsx
+++ src/__testfixtures__/Diacritics.output.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import { useTranslation } from 'react-i18next';
+import { useIntl } from 'react-intl';
 
 function Simple() {
-  const { t } = useTranslation();
-  return <span>{t('ola_antonio')}</span>;
+  const intl = useIntl();
+  return (
+    <span>{intl.formatMessage({
+      defaultMessage: 'Olá Antônio',
+      description: 'DESCRIBE_ABOVE_TEXT_HERE'
+    })}</span>
+  );
 }
 
 export default Simple;
