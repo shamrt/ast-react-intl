3eac1d4cc0845372b97249174a507c30dff46756
Shane Martin <shane.martin@plooto.com>
2024-01-05 11:45:48 -0500

    refactor: abstract FormatJS helpers into module

--- src/intlTransformerCodemod.ts
+++ src/intlTransformerCodemod.ts
@@ -1,232 +1,30 @@
-import { ASTNode } from 'ast-types';
-import { LiteralKind, PropertyKind } from 'ast-types/gen/kinds';
+import { PropertyKind } from 'ast-types/gen/kinds';
 import {
   API,
   CallExpression,
   ConditionalExpression,
   FileInfo,
-  Identifier,
   JSCodeshift,
   JSXElement,
   JSXExpressionContainer,
-  JSXFragment,
-  JSXIdentifier,
-  JSXText,
-  MemberExpression,
   Options,
-  Property,
-  StringLiteral,
 } from 'jscodeshift';
 import { Collection } from 'jscodeshift/src/Collection';
 import { NodePath } from 'ast-types/lib/node-path';
 import {
-  collapseInternalSpace,
   isWhitespace,
   canHandleAttribute,
   attributeLooksLikeText,
   findFunctionByIdentifier,
 } from './helpers/jscodeshift';
 import { hasStringLiteralArguments } from './visitorChecks';
-
-type ImportStatementOptions = {
-  componentUsed?: boolean;
-  hooksUsed?: boolean;
-  injectUsed?: boolean;
-};
-
-/** Returns an import statement for react-intl. */
-const getImportStatement = ({
-  componentUsed = false,
-  hooksUsed = false,
-  injectUsed = false,
-}: ImportStatementOptions = {}) => {
-  const namedImports = [];
-  if (componentUsed) {
-    namedImports.push('FormattedMessage');
-  }
-  if (hooksUsed) {
-    namedImports.push('useIntl');
-  }
-  if (injectUsed) {
-    namedImports.push('injectIntl');
-  }
-  if (namedImports.length > 0) {
-    return `import { ${namedImports.join(', ')} } from 'react-intl';`;
-  }
-  return '';
-};
-
-/** Adds an import statement for react-intl. */
-const addI18nImport = (
-  j: JSCodeshift,
-  root: Collection<unknown>,
-  importStatementOptions: ImportStatementOptions,
-) => {
-  const statement = getImportStatement(importStatementOptions);
-
-  const reactIntlImports = root
-    .find(j.ImportDeclaration)
-    .filter((path) => path.node.source.value === 'react-intl');
-
-  if (reactIntlImports.length > 0) {
-    return;
-  }
-
-  const imports = root.find(j.ImportDeclaration);
-
-  if (imports.length > 0) {
-    j(imports.at(imports.length - 1).get()).insertAfter(statement); // after the imports
-  } else {
-    root.get().node.program.body.unshift(statement); // beginning of file
-  }
-};
-
-/** Generates a safe parameter name in a FormatJS message descriptor. */
-const generateArgName = (expression: ASTNode, idx: number): string => {
-  if (expression.type === 'Identifier') {
-    return (expression as Identifier).name;
-  }
-  if (expression.type === 'MemberExpression') {
-    return generateArgName((expression as MemberExpression).property, idx);
-  }
-  return `arg${idx}`;
-};
-
-/** Generates a safe tag name for a FormatJS message descriptor. */
-const generateTagName = (element: JSXElement, idx: number): string => {
-  if ((element?.openingElement?.name as JSXIdentifier).name) {
-    return (element.openingElement.name as JSXIdentifier).name + idx;
-  }
-
-  return `tag${idx}`;
-};
-
-/** Generates a simple property for a FormatJS message descriptor. */
-const generateArgProps = (
-  j: JSCodeshift,
-  expression: JSXExpressionContainer,
-  argName: string,
-): Property => j.property('init', j.literal(argName), expression);
-
-/** Generates an arrow function property for a FormatJS message descriptor. */
-const generateTagProps = (
-  j: JSCodeshift,
-  element: JSXElement,
-  tagName: string,
-): Property => {
-  const arrowFn = j.arrowFunctionExpression(
-    [j.identifier('chunks')],
-    j.jsxElement(element.openingElement, element.closingElement, [
-      j.jsxExpressionContainer(j.identifier('chunks')),
-    ]),
-    false,
-  );
-
-  return j.property('init', j.literal(tagName), arrowFn);
-};
-
-/** Returns the text content of a JSX element with placeholders for parameters. */
-const getTextWithPlaceholders = (
-  j: JSCodeshift,
-  children: (
-    | JSXText
-    | StringLiteral
-    | JSXExpressionContainer
-    | JSXElement
-    | LiteralKind
-    | JSXFragment
-  )[],
-): [string, Property[], boolean] => {
-  let hasI18nUsage = false;
-  const text: string[] = [];
-  const properties: Property[] = [];
-  children.forEach((child, idx) => {
-    if (j.JSXText.check(child) || j.StringLiteral.check(child)) {
-      if (!isWhitespace(child.value)) {
-        hasI18nUsage = true;
-      }
-
-      text.push(child.value);
-    } else if (
-      j.JSXExpressionContainer.check(child) &&
-      !j.JSXEmptyExpression.check(child.expression)
-    ) {
-      const argName = generateArgName(child.expression, idx);
-      const placeholderProps = generateArgProps(
-        j,
-        child.expression as JSXExpressionContainer,
-        argName,
-      );
-      text.push(`{${argName}}`);
-      properties.push(placeholderProps);
-    } else if (j.JSXElement.check(child)) {
-      const tagName = generateTagName(child, idx);
-      const tagProps = generateTagProps(j, child, tagName);
-      // @ts-expect-error: TypeScript thinks `children` is undefined, but that's wrong
-      const [childText, moreProps] = getTextWithPlaceholders(j, child.children);
-      text.push(`<${tagName}>${childText}</${tagName}>`);
-      properties.push(tagProps);
-      properties.push(...moreProps);
-    }
-  });
-  return [
-    collapseInternalSpace(text.join('')).trim(),
-    properties,
-    hasI18nUsage,
-  ];
-};
-
-/**
- * Generates a call to `intl.formatMessage` with the given text and parameters.
- */
-function generateIntlCall(
-  j: JSCodeshift,
-  text: string,
-  params: Property[] = [],
-) {
-  const intlCallParams = [];
-  intlCallParams.push(
-    j.objectExpression([
-      j.property('init', j.identifier('defaultMessage'), j.literal(text)),
-    ]),
-  );
-  if (params?.length) {
-    intlCallParams.push(j.objectExpression(params));
-  }
-  return j.callExpression(
-    j.memberExpression(
-      j.identifier('intl'),
-      j.identifier('formatMessage'),
-      false,
-    ),
-
-    intlCallParams,
-  );
-}
-
-/** Creates a `const intl = useIntl()` statement. */
-function createUseIntlCall(j: JSCodeshift) {
-  return j.variableDeclaration('const', [
-    j.variableDeclarator(
-      j.identifier('intl'),
-      j.callExpression(j.identifier('useIntl'), []),
-    ),
-  ]);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function addUseHookToFunctionBody(j: JSCodeshift, functions: Collection<any>) {
-  let hookFound = false;
-  functions.forEach((n) => {
-    hookFound = true;
-    const { body } = n.node;
-    // eslint-disable-next-line no-param-reassign
-    n.node.body = j.BlockStatement.check(body)
-      ? j.blockStatement([createUseIntlCall(j), ...body.body])
-      : j.blockStatement([createUseIntlCall(j), j.returnStatement(body)]);
-  });
-  return hookFound;
-}
+import {
+  getTextWithPlaceholders,
+  generateIntlCall,
+  addUseHookToFunctionBody,
+  createUseIntlCall,
+  addI18nImport,
+} from './helpers/formatjs';
 
 // <span>test</span>
 // <span>test {value}</span>
