61bc82ed7d243991c328aa82ee7d9be0c80ef304
Shane Martin <shane.martin@plooto.com>
2024-01-08 14:13:11 -0500

    test: update expected `FunctionArguments.output`

--- src/__testfixtures__/FunctionArguments.output.tsx
+++ src/__testfixtures__/FunctionArguments.output.tsx
@@ -4,21 +4,23 @@ import { useIntl } from 'react-intl';
 
 function FunctionArguments() {
   const intl = useIntl();
-  const typicalFunction = String('bar')
+  const typicalFunction = String('bar');
   const functionWithStringArg = String(intl.formatMessage({
     defaultMessage: 'My simple text'
-  }))
-  const functionWithObjectNonTextArg = String({ foo: 'bar' })
+  }));
+  const functionWithObjectNonTextArg = String({ foo: 'bar' });
   const functionWithObjectTextArg = String({ title: intl.formatMessage({
     defaultMessage: 'My simple text'
-  }) })
+  }) });
 
-  return <div>
-    {typicalFunction}
-    {functionWithStringArg}
-    {functionWithObjectNonTextArg}
-    {functionWithObjectTextArg}
-  </div>
+  return (
+    <div>
+      {typicalFunction}
+      {functionWithStringArg}
+      {functionWithObjectNonTextArg}
+      {functionWithObjectTextArg}
+    </div>
+  );
 }
 
 export default FunctionArguments;
